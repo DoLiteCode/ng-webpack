2cb6a26511be448f487e0a2b819870c3e6f9d27b
Shakil Hossain <shakil.hossain.bgd@gmail.com>
2019-05-22 15:56:45 +0600

    project init

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { HttpModule } from "@angular/http";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { ServersComponent } from "./servers/servers.component";
-import { ServerComponent } from "./server/server.component";
 
 
 @NgModule({
@@ -18,9 +16,7 @@ import { ServerComponent } from "./server/server.component";
     FormsModule
   ],
   declarations: [
-    AppComponent,
-    ServersComponent,
-    ServerComponent
+    AppComponent
   ]
 })
 
